227293fef6a454b88d2d7c6174ccca2029234e8f
matthiasvanwoensel <matthias@fluvio.be>
2022-06-22 10:57:33 +0200

    implementatie progressbar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,30 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var EggProgressBar: UIProgressView!
     @IBOutlet weak var TextViewEggs: UILabel!
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var secondsRemaining = 60
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+    
         timer.invalidate()
         
         TextViewEggs.text = "How do you like your eggs?"
         
-        
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime{
+            
+            let percentageProgress = secondsPassed / totalTime
+            
+            EggProgressBar.progress = Float(percentageProgress)
+        
+            secondsPassed += 1
         }else{
             timer.invalidate()
             TextViewEggs.text = "DONE!"
